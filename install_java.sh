7c9bb1406a0479c07b4e37ca74f0437d10e0b010
Bernard <mainabernardpro@gmail.com>
2024-07-08 11:21:37 +0300

    Added Scripts...

--- install_java.sh
+++ install_java.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-
-# Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
-
-# Add the Oracle Java PPA
 sudo add-apt-repository ppa:linuxuprising/java -y
-
-# Update the package list again
 sudo apt update
-
-# Install Oracle Java 17
-sudo apt-get install oracle-java17-installer oracle-java17-set-default -y
-
-# Verify the installation
+sudo apt install default-jdk -y
 java -version
